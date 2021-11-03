67f5f071ef2a235c1e412787b30d95f56a91abd1
Bryson Marshall <bryson.marshall@stu.marshall.kyschools.us>
2021-11-03 16:35:16 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+app.use(express.static('.'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.get('/status', async (req, res) => {
+    res.status(200)
+    res.send("All Good!")
+})
+app.get('/home', async(req, res) => {
+    res.sendFile(path.join(__dirname + '/pages/home.html'))
+})
+app.get('/about-us', async(req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/aboutus.html'))
+})
+app.get('/img/:name/:extent',async(req, res)=>{
+    res.sendFile(path.join(__dirname + '/assets/'+req.params.name+'.'+req.params.extent))
+}) 
+app.get('/styleSheet/:name', async(req, res)=>{
+    res.sendFile(path.join(__dirname + '/styles/'+req.params.name+'.css'))
+})
+app.listen(4242, () => console.log('Running on port 4242'));
