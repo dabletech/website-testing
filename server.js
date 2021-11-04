d60d9482a1823a1afbd203418ba98b65a3567cbb
Bryson Marshall <bryson.marshall@stu.marshall.kyschools.us>
2021-11-04 12:09:39 -0500

    fixed a corner

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path')
+const crypto = require('crypto')
 const {Deta} = require('deta');
 const app = express();
 const deta = Deta('b0bj02xu_yJxh7WpXiJEcGiZkc2GZtW4aJ59UcMs8');
@@ -7,7 +8,7 @@ const productDatabase = deta.Base("Products")
 app.use(express.static('.'));
 var port = process.env.PORT || 3000;
 app.listen(port);
-const authKey = ""
+const authKey = "unOYa%faNJ1v_4M#tY*A=lMQZM8gWlX5N#tMUPohAO0g1kYzj*rY7c25gVPpXo%5n0y|0Mwlo|X#=Yr??d-Zv+%sB@OPO_2HLkmjLanZ2L&P7BVY4_2N4+j4E9P#pzxMB?kuiFS7@CSldYRrk?djKcRqD1Et67&-v7PKea9yTom7h&M-+iGy=9Wi0hsx8pG6Ib@52!YtGqgpOtKwA=Dvd8KNj#zej&PG=mpmdZ87d@50_KcoOg8FEWD_pc-G4fek"
 const publicAuthKey = "?b5X-+#D|Kfq^LZk"
 const YOUR_DOMAIN = 'http://localhost:80';
 
@@ -39,6 +40,6 @@ app.get('/styleSheet/:name', async(req, res)=>{
 app.post('/add-item-to-cart/:userId', async(req,res)=>{
 
 })
-app.post('/newUser', async(req,res)=>{
+app.post('/new-user', async(req,res)=>{
     
 })
