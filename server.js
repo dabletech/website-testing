79dd8fcf5197c8d8ec9b1d22df6212ac016b3dfc
Bryson Marshall <bryson.marshall@stu.marshall.kyschools.us>
2021-11-04 10:22:57 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path')
+const {Deta} = require('deta');
 const app = express();
+const deta = Deta('b0bj02xu_yJxh7WpXiJEcGiZkc2GZtW4aJ59UcMs8');
+const productDatabase = deta.Base("Products")
 app.use(express.static('.'));
 var port = process.env.PORT || 3000;
 app.listen(port);
-
+const authKey = ""
+const publicAuthKey = "?b5X-+#D|Kfq^LZk"
 const YOUR_DOMAIN = 'http://localhost:80';
 
 app.get('/status', async (req, res) => {
@@ -23,10 +27,18 @@ app.get('/about-us', async(req, res)=>{
 app.get('/learn-more', async(req, res)=>{
     res.sendFile(path.join(__dirname + '/pages/learnmore.html'))
 })
+app.get('/checkout', async(req, res)=>{
+    res.sendFile(path.join(__dirname + '/pages/checkout.html'))
+})
 app.get('/img/:name/:extent',async(req, res)=>{
     res.sendFile(path.join(__dirname + '/assets/'+req.params.name+'.'+req.params.extent))
 }) 
 app.get('/styleSheet/:name', async(req, res)=>{
     res.sendFile(path.join(__dirname + '/styles/'+req.params.name+'.css'))
 })
+app.post('/add-item-to-cart/:userId', async(req,res)=>{
 
+})
+app.post('/newUser', async(req,res)=>{
+    
+})
